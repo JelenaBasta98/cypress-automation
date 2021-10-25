43f6a9583455ba95c010fae8ac42134aa16c6809
Jelena <jelenabasta98@gmail.com>
2021-10-25 11:56:43 +0200

    initial commit

---
+++ cypress/integration/guru99-bank/testLogin.spec.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+
+describe("Testing Guru99 Bank", () => {
+
+    it("Testing Login functionality", () => {
+
+        cy.visit("http://demo.guru99.com/V4/");
+
+        cy.get('input[name="uid"]').type("mngr358461");
+       
+        //cy.get().type("sUvUbeq");
+        
+
+    })
+
+
+})
